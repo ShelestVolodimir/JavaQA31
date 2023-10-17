e55cafbf49b3056ee5d984b4a57af7d4ea4ab6e0
38098 <selestvova1313@gmail.com>
2023-10-17 16:52:55 +0300

    DzLesson5

--- src/main/java/org/example/Lesson5/Hw2ls5.java
+++ src/main/java/org/example/Lesson5/Hw2ls5.java
@@ -9,19 +9,16 @@ public class Hw2ls5 {
         System.out.print("Введіть розмір сторони квадрата: ");
         int size = scanner.nextInt();
 
-        // Виведення незаповненого квадрата
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
-                // Перевірка, чи поточний рядок або стовпець - крайній
+
                 if (i == 1 || i == size || j == 1 || j == size) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
-            System.out.println(); // Перехід на новий рядок після кожного рядка квадрата
+            System.out.println();
         }
-
-        scanner.close();
     }
 }
