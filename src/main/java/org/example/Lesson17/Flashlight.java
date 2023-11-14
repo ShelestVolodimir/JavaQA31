5a6c632d77f7a437e3723175b6d60551b5dbdfaf
38098 <selestvova1313@gmail.com>
2023-11-14 21:26:16 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/org/example/Lesson17/Flashlight.java
@@ -0,0 +1,12 @@
+package org.example.Lesson17;
+
+public class Flashlight {
+    public String isLight() {
+    }
+
+    public void off() {
+    }
+
+    public void on() {
+    }
+}
---
+++ src/main/java/org/example/Lesson17/Hw17.java
@@ -0,0 +1,47 @@
+package org.example.Lesson17;
+
+public class Hw17 {
+    private boolean light;
+
+    public void Flashlight() {
+        // Ліхтарик вимкнений при створенні
+        this.light = false;
+    }
+
+    // Метод для ввімкнення ліхтарика
+    public void on() {
+        light = true;
+        System.out.println("Ліхтарик увімкнено.");
+    }
+
+    // Метод для вимкнення ліхтарика
+    public void off() {
+        light = false;
+        System.out.println("Ліхтарик вимкнено.");
+    }
+
+    // Метод для перевірки стану ліхтарика
+    public boolean isLight() {
+        return light;
+    }
+
+    public static void main(String[] args) {
+        Flashlight flashlight = new Flashlight();
+
+        // Перевірка стану ліхтарика
+        System.out.println("Ліхтарик увімкнено? " + flashlight.isLight());
+
+        // Ввімкнення ліхтарика
+        flashlight.on();
+
+        // Перевірка стану ліхтарика після ввімкнення
+        System.out.println("Ліхтарик увімкнено? " + flashlight.isLight());
+
+        // Вимкнення ліхтарика
+        flashlight.off();
+
+        // Перевірка стану ліхтарика після вимкнення
+        System.out.println("Ліхтарик увімкнено? " + flashlight.isLight());
+    }
+}
+}
